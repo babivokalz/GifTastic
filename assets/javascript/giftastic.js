4c9e1fa183577e7e3874902dd89106e89daa7eb6
AlaCrity <wlchen1105@gmail.com>
2019-06-18 23:28:33 -0400

    wrote the javascript code and linked the giphy API to successfully call on gifs to appear based on dog breed

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,3 +1,14 @@
 $('button').on('click', function() {
-    
+    var dog = $(this).attr('data-dog');
+    var queryURL =
+          "https://api.giphy.com/v1/gifs/search?q=" +
+          dog +
+          "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    $.ajax({url: queryURL, method: 'GET'}).then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = 
+        }
+    })
 }
