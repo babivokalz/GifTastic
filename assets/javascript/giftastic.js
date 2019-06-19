3597c0b302d66edc80971ca5f822a82fcf9cc098
AlaCrity <wlchen1105@gmail.com>
2019-06-19 00:02:46 -0400

    added javascript code to create new buttons

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,14 +1,34 @@
-$('button').on('click', function() {
-    var dog = $(this).attr('data-dog');
-    var queryURL =
-          "https://api.giphy.com/v1/gifs/search?q=" +
-          dog +
-          "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-    $.ajax({url: queryURL, method: 'GET'}).then(function(response) {
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = 
-        }
-    })
+function createButton() {
+  $("#buttons").empty();
+  var newButton = $("<button>");
+  newButton.attr("type", "button");
+  newButton.attr("class", "button-print btn btn-2 btn-2h");
+  newButton.attr("id");
+  $("#buttons").append(newButton);
 }
+$("button").on("click", function() {
+  var dog = $(this).attr("data-dog");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    dog +
+    "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  $.ajax({ url: queryURL, method: "GET" }).then(function(response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div>");
+      var rating = results[i].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      var dogImage = $("<img>");
+
+      dogImage.attr("src", results[i].images.fixed_height.url);
+      dogImage.attr("url_movie", data.images.original.url);
+      dogImage.attr("url_still", data.images.original_still.url);
+
+      gifDiv.prepend(p);
+      gifDiv.prepend(dogImage);
+
+      $("#gifs-go-here").prepend(gifDiv);
+    }
+  });
+});
